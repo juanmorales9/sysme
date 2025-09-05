6a92e14552d889d3c393cfce0982fff4c3b496b6
juanmorales9 <66259831+juanmorales9@users.noreply.github.com>
2025-09-05 20:27:23 -0300

    Delete renderer/vite.config.js

--- renderer/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "renderer",   // 👈 Aquí se guardará el index.html y los assets
-  },
-});
